e257cbd646fba4663f487fa97b7d17eafd9b56dd
praxand <jay.cheung2005@gmail.com>
2023-12-05 11:00:59 +0100

    home page wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
   <div>index</div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: "Jay Cheung",
+  ogTitle: "Jay Cheung",
+  description: "Jay Cheung's personal website",
+  ogDescription: "Jay Cheung's personal website",
+  ogImage: "https://jaycheung.nl/profile.png",
+  twitterCard: 'summary_large_image',
+});
+</script>
