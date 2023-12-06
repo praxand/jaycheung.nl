714f503abb85cb86cd935e516dce4334f1ebee23
praxand <jay.cheung2005@gmail.com>
2023-12-06 11:31:33 +0100

    fix pls pls pls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
       meta: [
         {
           name: "viewport",
-          content: "width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no",
+          content: "width=device-width, minimum-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no",
         },
         {
           name: "theme-color",
