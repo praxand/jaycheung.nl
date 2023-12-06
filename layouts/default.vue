5dfd964d024e4cdbdc0289bb618ef1f8aabd59ac
praxand <jay.cheung2005@gmail.com>
2023-12-06 10:09:42 +0100

    container

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
-    <slot />
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-3xl">
+        <slot />
+      </div>
+    </div>
   </main>
 </template>
