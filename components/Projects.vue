05b6a297ea3d5691ddee4542d060abf03c81f002
praxand <jay.cheung2005@gmail.com>
2023-12-06 11:44:41 +0100

    wip

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,69 +1,45 @@
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
--->
 <template>
-  <div class="bg-white">
-    <div
-      class="mx-auto max-w-2xl px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8"
-    >
-      <div class="mx-auto max-w-3xl text-center">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Protect your device
-        </h2>
-        <p class="mt-4 text-gray-500">
-          As a digital creative, your laptop or tablet is at the center of your
-          work. Keep your device safe with a fabric sleeve that matches in
-          quality and looks.
-        </p>
-      </div>
+  <div class="mx-auto max-w-2xl px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
+    <div class="mx-auto max-w-3xl text-center">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Projects
+      </h2>
+    </div>
 
-      <div class="mt-16 space-y-16">
+    <div class="mt-16 space-y-16">
+      <div
+        v-for="(feature, featureIdx) in features"
+        :key="feature.name"
+        class="flex flex-col-reverse lg:grid lg:grid-cols-12 lg:items-center lg:gap-x-8"
+      >
         <div
-          v-for="(feature, featureIdx) in features"
-          :key="feature.name"
-          class="flex flex-col-reverse lg:grid lg:grid-cols-12 lg:items-center lg:gap-x-8"
+          :class="[
+            featureIdx % 2 === 0
+              ? 'lg:col-start-1'
+              : 'lg:col-start-8 xl:col-start-9',
+            'mt-6 lg:col-span-5 lg:row-start-1 lg:mt-0 xl:col-span-4',
+          ]"
         >
-          <div
-            :class="[
-              featureIdx % 2 === 0
-                ? 'lg:col-start-1'
-                : 'lg:col-start-8 xl:col-start-9',
-              'mt-6 lg:col-span-5 lg:row-start-1 lg:mt-0 xl:col-span-4',
-            ]"
-          >
-            <h3 class="text-lg font-medium text-gray-900">
-              {{ feature.name }}
-            </h3>
-            <p class="mt-2 text-sm text-gray-500">{{ feature.description }}</p>
-          </div>
-          <div
-            :class="[
-              featureIdx % 2 === 0
-                ? 'lg:col-start-6 xl:col-start-5'
-                : 'lg:col-start-1',
-              'flex-auto lg:col-span-7 lg:row-start-1 xl:col-span-8',
-            ]"
-          >
-            <div
-              class="aspect-h-2 aspect-w-5 overflow-hidden rounded-lg bg-gray-100"
-            >
-              <img
-                :src="feature.imageSrc"
-                :alt="feature.imageAlt"
-                class="object-cover object-center"
-              />
-            </div>
+          <h3 class="text-lg font-medium text-gray-900">
+            {{ feature.name }}
+          </h3>
+          <p class="mt-2 text-sm text-gray-500">{{ feature.description }}</p>
+        </div>
+
+        <div
+          :class="[
+            featureIdx % 2 === 0
+              ? 'lg:col-start-6 xl:col-start-5'
+              : 'lg:col-start-1',
+            'flex-auto lg:col-span-7 lg:row-start-1 xl:col-span-8',
+          ]"
+        >
+          <div class="aspect-h-2 aspect-w-5 overflow-hidden rounded-lg bg-gray-100">
+            <img
+              :src="feature.imageSrc"
+              :alt="feature.imageAlt"
+              class="object-cover object-center"
+            />
           </div>
         </div>
       </div>
@@ -74,21 +50,20 @@
 <script setup>
 const features = [
   {
-    name: "Minimal and thoughtful",
+    name: "Lorem ipsum",
     description:
-      'Our laptop sleeve is compact and precisely fits 13" devices. The zipper allows you to access the interior with ease, and the front pouch provides a convenient place for your charger cable.',
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores debitis, inventore praesentium ut ad reiciendis optio est aut, eaque fugit nobis non iure molestias nihil repudiandae vero assumenda officiis porro?",
     imageSrc:
       "https://tailwindui.com/img/ecommerce-images/product-feature-07-detail-01.jpg",
-    imageAlt:
-      "White canvas laptop sleeve with gray felt interior, silver zipper, and tan leather zipper pull.",
+    imageAlt: "Lorem ipsum",
   },
   {
-    name: "Refined details",
+    name: "Lorem ipsum",
     description:
-      "We design every detail with the best materials and finishes. This laptop sleeve features durable canvas with double-stitched construction, a felt interior, and a high quality zipper that hold up to daily use.",
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores debitis, inventore praesentium ut ad reiciendis optio est aut, eaque fugit nobis non iure molestias nihil repudiandae vero assumenda officiis porro?",
     imageSrc:
       "https://tailwindui.com/img/ecommerce-images/product-feature-07-detail-02.jpg",
-    imageAlt: "Detail of zipper pull with tan leather and silver rivet.",
+    imageAlt: "Lorem ipsum",
   },
 ];
 </script>
